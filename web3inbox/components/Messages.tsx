7b75ab1c5ac5b59b96119a5b1226511c9308ba5c
Nikita Varabei <nVarabei@gmail.com>
2023-09-23 16:13:27 -0400

    revert messages screen

--- web3inbox/components/Messages.tsx
+++ web3inbox/components/Messages.tsx
@@ -27,64 +27,72 @@ function Messages() {
   console.log("messages", messages);
 
   return (
-    <Box overflowY="scroll" position={"relative"} maxH="400px">
-      <AccordionPanel
-        display="flex"
-        flexDirection={"column"}
-        pb={4}
-        gap={2}
-        position={"relative"}
-      >
-        {!messages?.length ? (
-          <Text>No messages yet.</Text>
-        ) : (
-          messages
-            .sort((a, b) => b.id - a.id)
-            .filter(({ message }) => message.type === "alerts")
-            .map(({ id, message }) => (
-              <Alert
-                as={Link}
-                href={message.url}
-                target="_blank"
-                key={id}
-                status="info"
-                colorScheme={
-                  message.type === "transactional" ? "blue" : "purple"
-                }
-                rounded="xl"
-              >
-                <AlertIcon />
+    <AccordionItem>
+      <AccordionButton>
+        <Heading fontSize="md" as="span" flex="1" textAlign="left">
+          Last Messages
+        </Heading>
+        <AccordionIcon />
+      </AccordionButton>
+      <Box overflowY="scroll" position={"relative"} maxH="400px">
+        <AccordionPanel
+          display="flex"
+          flexDirection={"column"}
+          pb={4}
+          gap={2}
+          position={"relative"}
+        >
+          {!messages?.length ? (
+            <Text>No messages yet.</Text>
+          ) : (
+            messages
+              .sort((a, b) => b.id - a.id)
+              .filter(({ message }) => message.type === "alerts")
+              .map(({ id, message }) => (
+                <Alert
+                  as={Link}
+                  href={message.url}
+                  target="_blank"
+                  key={id}
+                  status="info"
+                  colorScheme={
+                    message.type === "transactional" ? "blue" : "purple"
+                  }
+                  rounded="xl"
+                >
+                  <AlertIcon />
 
-                <Flex flexDir={"column"} flexGrow={1}>
-                  <AlertTitle>{message.title}</AlertTitle>
-                  <AlertDescription flexGrow={1}>
-                    {message.body}
-                  </AlertDescription>
-                </Flex>
-                <Flex w="60px" justifyContent="center">
-                  <Image
-                    src={message.icon}
-                    alt="notification image"
-                    height="60px"
-                    rounded="full"
-                    alignSelf="center"
+                  <Flex flexDir={"column"} flexGrow={1}>
+                    <AlertTitle>{message.title}</AlertTitle>
+                    <AlertDescription flexGrow={1}>
+                      {message.body}
+                    </AlertDescription>
+                  </Flex>
+                  <Flex w="60px" justifyContent="center">
+                    <Image
+                      src={message.icon}
+                      alt="notification image"
+                      height="60px"
+                      rounded="full"
+                      alignSelf="center"
+                    />
+                  </Flex>
+                  <CloseButton
+                    alignSelf="flex-start"
+                    position="relative"
+                    right={-1}
+                    top={-1}
+                    onClick={async (e) => {
+                      e.preventDefault();
+                      deleteMessage(id);
+                    }}
                   />
-                </Flex>
-                <CloseButton
-                  alignSelf="flex-start"
-                  position="relative"
-                  right={-1}
-                  top={-1}
-                  onClick={async (e) => {
-                    e.preventDefault();
-                    deleteMessage(id);
-                  }}
-                />
-              </Alert>
-            ))
-        )}
-      </AccordionPanel>
-    </Box>
+                </Alert>
+              ))
+          )}
+        </AccordionPanel>
+      </Box>
+    </AccordionItem>
   );
 }
 
