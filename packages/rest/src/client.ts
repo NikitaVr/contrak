946cecc18cf83dc6126a6d6bee839653bc046570
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-09-23 10:59:28 -0400

    update base url

--- packages/rest/src/client.ts
+++ packages/rest/src/client.ts
@@ -2,7 +2,7 @@ import { initClient } from "@ts-rest/core";
 import { contract } from "./contract";
 
 const client = initClient(contract, {
-  baseUrl: "/api",
+  baseUrl: "http://localhost:3000/api",
   baseHeaders: {
     "Content-Type": "application/json",
   },
