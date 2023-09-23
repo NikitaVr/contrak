9fcdaa9649cc83b52ceb25c7a45e989f6198857e
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-09-23 16:23:46 -0400

    point rest client to deployed version for now

--- packages/rest/src/client.ts
+++ packages/rest/src/client.ts
@@ -2,7 +2,7 @@ import { initClient } from "@ts-rest/core";
 import { contract } from "./contract";
 
 const client = initClient(contract, {
-  baseUrl: "http://localhost:3000/api",
+  baseUrl: "https://midna-production.up.railway.app/api",
   baseHeaders: {
     "Content-Type": "application/json",
   },
