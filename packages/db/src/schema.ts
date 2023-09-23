ee3de065956c877d11a266783105a8445db2f54c
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-09-23 15:34:24 -0400

    remove unique constraint

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -5,22 +5,16 @@ import {
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 
-export const contracts = sqliteTable(
-  "contracts",
-  {
-    id: integer("id").primaryKey({ autoIncrement: true }),
-    name: text("name").notNull(),
-    chainId: text("chain_id").notNull(),
-    contractAddress: text("contract_address").notNull(),
-    deployerAddress: text("deployer_address"),
-    deploymentTransactionHash: text("deployment_transaction_hash"),
-    orgPublicKey: text("org_public_key"),
-    message: text("message"),
-    deployerSignature: text("deployer_signature"),
-    orgSignature: text("org_signature"),
-    createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
-  },
-  (contracts) => ({
-    nameIdx: uniqueIndex("nameIdx").on(contracts.name),
-  })
-);
+export const contracts = sqliteTable("contracts", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  chainId: text("chain_id").notNull(),
+  contractAddress: text("contract_address").notNull(),
+  deployerAddress: text("deployer_address"),
+  deploymentTransactionHash: text("deployment_transaction_hash"),
+  orgPublicKey: text("org_public_key"),
+  message: text("message"),
+  deployerSignature: text("deployer_signature"),
+  orgSignature: text("org_signature"),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+});
