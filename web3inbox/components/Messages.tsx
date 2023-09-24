d94bdb11996d002dff74d266c1d939e28d996aed
Nikita Varabei <nVarabei@gmail.com>
2023-09-23 22:20:31 -0400

    update web3inbox UI

--- web3inbox/components/Messages.tsx
+++ web3inbox/components/Messages.tsx
@@ -10,6 +10,7 @@ import {
   Box,
   CloseButton,
   Flex,
+  HStack,
   Heading,
   Image,
   Text,
@@ -30,46 +31,50 @@ function Messages() {
     return <Text>No messages yet.</Text>;
   }
 
-  return messages
-    .sort((a, b) => b.id - a.id)
-    .filter(({ message }) => message.type === "alerts")
-    .map(({ id, message }) => (
-      <Alert
-        as={Link}
-        href={message.url}
-        target="_blank"
-        key={id}
-        status="info"
-        colorScheme={message.type === "transactional" ? "blue" : "purple"}
-        rounded="xl"
-      >
-        <AlertIcon />
+  return (
+    <HStack spacing={1}>
+      {messages
+        .sort((a, b) => b.id - a.id)
+        .filter(({ message }) => message.type === "alerts")
+        .map(({ id, message }) => (
+          <Alert
+            as={Link}
+            href={message.url}
+            target="_blank"
+            key={id}
+            status="info"
+            colorScheme={message.type === "transactional" ? "blue" : "purple"}
+            rounded="xl"
+          >
+            <AlertIcon />
 
-        <Flex flexDir={"column"} flexGrow={1}>
-          <AlertTitle>{message.title}</AlertTitle>
-          <AlertDescription flexGrow={1}>{message.body}</AlertDescription>
-        </Flex>
-        <Flex w="60px" justifyContent="center">
-          <Image
-            src={message.icon}
-            alt="notification image"
-            height="60px"
-            rounded="full"
-            alignSelf="center"
-          />
-        </Flex>
-        <CloseButton
-          alignSelf="flex-start"
-          position="relative"
-          right={-1}
-          top={-1}
-          onClick={async (e) => {
-            e.preventDefault();
-            deleteMessage(id);
-          }}
-        />
-      </Alert>
-    ));
+            <Flex flexDir={"column"} flexGrow={1}>
+              <AlertTitle>{message.title}</AlertTitle>
+              <AlertDescription flexGrow={1}>{message.body}</AlertDescription>
+            </Flex>
+            <Flex w="60px" justifyContent="center">
+              <Image
+                src={message.icon}
+                alt="notification image"
+                height="60px"
+                rounded="full"
+                alignSelf="center"
+              />
+            </Flex>
+            <CloseButton
+              alignSelf="flex-start"
+              position="relative"
+              right={-1}
+              top={-1}
+              onClick={async (e) => {
+                e.preventDefault();
+                deleteMessage(id);
+              }}
+            />
+          </Alert>
+        ))}
+    </HStack>
+  );
 }
 
 export default Messages;
