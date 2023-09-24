b621d420084da0d4e55dbfa04307f72bdb51a67b
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-09-24 07:56:48 -0400

    console.log database url

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,6 +2,8 @@ import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env.DATABASE_URL);
+
 export default {
   schema: "./src/schema.ts",
   driver: "better-sqlite",
