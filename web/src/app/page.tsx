751e2659dfa7a04345e0e0ca54ba4bd387ccfa4a
Nikita Varabei <nVarabei@gmail.com>
2023-10-16 22:37:32 -0400

    adjust styling

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="py-16">
       <div className="px-4 mx-auto w-full max-w-screen-md flex flex-col items-start gap-8">
         <div className="relative w-full flex flex-col gap-4 items-start">
-          <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl tracking-[.3em]">
+          <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl tracking-[.2em]">
             <i>CONTRAK</i>
           </h1>
           <p className="leading-7 [&:not(:first-child)]:mt-2">
