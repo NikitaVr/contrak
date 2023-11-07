a75b90fd307eafe4db03e1f1e47d0ebb7ed0d1a2
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-11-07 16:09:49 -0500

    load env vars

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,13 +1,18 @@
 import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
-dotenv.config();
 
-console.log(process.env.DATABASE_URL);
+dotenv.config({
+  path: "../../.env",
+});
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
 
 export default {
   schema: "./src/schema.ts",
   driver: "better-sqlite",
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? "../../data.db",
+    url: process.env.DATABASE_URL,
   },
 } satisfies Config;
