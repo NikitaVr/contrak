82a24320852cb51af64ba6adb55c3fb2eec65bf8
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-11-08 16:09:49 -0500

    build for common js and esm

--- packages/sdk/tsup.config.ts
+++ packages/sdk/tsup.config.ts
@@ -5,6 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   name: "sdk",
   entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
   minify: isProduction,
   sourcemap: true,
   bundle: true,
